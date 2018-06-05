7dde56237ef7b222a76db3f5897304ab9b57b0f7
Kendall Kling <kkling@uncc.edu>
2018-06-05 10:12:47 -0400

    Submit Button Works!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('testing')
+
+const button = document.querySelector('button')
+
+function changeHeading() {
+  const heading = document.querySelector('#hd2')
+  heading.textContent = 'New Spellbook'
+}
+
+button.addEventListener('click', changeHeading)
+
+const button2 = document.querySelector('#Submit')
+function inputedHeading(){
+  const newName = document.querySelector('#hd2')
+  const inputedValue = document.querySelector("#newText").value
+  newName.textContent = inputedValue
+}
+
+button2.addEventListener('click', inputedHeading)
